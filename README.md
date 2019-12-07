08090021910aa24af006565dcc0c406541f87f25
gijoegotyler <33065655+gijoegotyler@users.noreply.github.com>
2019-12-07 15:56:37 -0700

    Added Enable Firewall Profiles, Updates to README

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Written in Powershell, designed specifically for Windows 10
 ## Features
 * Stop Features Using Text File (stopservices.txt)
 * Start Features Using Text File (startservices.txt)
-
+* Enables the firewall profiles for Domain, Private, and Public
+ 
 ### Warning, this script cannot be used by any team other than Team Mirai (12-1719) for Cyberpatriot
--- protecht.ps1
+++ protecht.ps1
@@ -1,10 +1,17 @@
+# gets the location of the scripts
 $location = Get-Location
+
+# stops every service in the stopservices.txt file
 foreach($line in [System.IO.File]::ReadLines("$location\stopservices.txt")) {
        Stop-Service -Name $line
 }
 
+# starts every service in the startservices.txt file
 foreach($line in [System.IO.File]::ReadLines("$location\startservices.txt")) {
 	Start-Service -Name $line
 }
+
+# Enables the firewall
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 Start-Sleep -s 10
